d3005f95c06b97d72c7dbc9c426fdaf23f398e19
cdmsn <cdmsn@users.noreply.github.com>
2019-12-06 12:00:09 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,7 +34,7 @@ class goagent (
     comment  => 'Repor for GoCD',
     location => 'http://download.gocd.org',
     release  => '',
-    repo     => '/',
+    repos     => '/',
     key      => {
        id      => '322259C82D3082B3E32AEC2ED8843F288816C449',
        server  => 'download.gocd.org',
